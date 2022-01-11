e96775e9003de650406b295886c37ad40fc4282f
Javi Romero <javiromero@elmurci.local>
2022-01-11 13:08:44 +0100

    fix(env): AWS env variables

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
 const AWS = require('aws-sdk');
 
 try {
   const functionName = core.getInput('LAMBDA_FUNCTION_NAME')
   const imageUri = core.getInput('ECR_IMAGE_URI')
-  const AWS_SECRET_KEY = core.getInput('AWS_SECRET_KEY');
-  const AWS_SECRET_ID = core.getInput('AWS_SECRET_ID');
-  const AWS_REGION = core.getInput('AWS_REGION');
 
   console.log(`Updating Function Name ${functionName} with ${imageUri}!`);
 
@@ -16,9 +11,9 @@ try {
 
   const lambda = new AWS.Lambda({
       apiVersion: '2015-03-31',
-      region: AWS_REGION,
-      secretAccessKey: AWS_SECRET_KEY,
-      accessKeyId: AWS_SECRET_ID,
+      region: env.AWS_REGION,
+      secretAccessKey: env.AWS_SECRET_KEY,
+      accessKeyId: env.AWS_SECRET_ID,
       maxRetries: 3,
       sslEnabled: true,
       logger: console,
